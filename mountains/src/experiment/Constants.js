4594b4e37aef52958f9b3e5a9e72fd8108b4cb42
Florian Morel <ayamflow@gmail.com>
2013-12-18 22:12:38 +0100

    Played with light and stuff

--- mountains/src/experiment/Constants.js
+++ mountains/src/experiment/Constants.js
@@ -1,5 +1,9 @@
 var Constants = {
     planeAngleX: -1.27,
     planeAngleY: 0.05,
-    planeAngleZ: Math.PI / 3
+    planeAngleZ: Math.PI / 3,
+
+    spotLightX: 100,
+    spotLightY: 100,
+    spotLightZ: 100
 };
--- mountains/src/experiment/Playground.js
+++ mountains/src/experiment/Playground.js
@@ -17,6 +17,17 @@ var Playground = function()
 Playground.prototype = {
     init: function()
     {
+        this.initRenderer();
+
+        this.quadNumbers = 20;
+
+        this.initMeshes();
+        this.initLights();
+
+        this.controls = new THREE.TrackballControls(this.camera);
+    },
+
+    initRenderer: function() {
         this.resize = new Resize();
         this.renderer = new THREE.WebGLRenderer();
         this.camera = new THREE.PerspectiveCamera(45, this.resize.screenWidth / this.resize.screenHeight, 0.1, 10000);
@@ -27,29 +38,48 @@ Playground.prototype = {
 
         this.renderer.setSize(this.resize.screenWidth, this.resize.screenHeight);
 
-        document.body.appendChild(this.renderer.domElement);
+        this.renderer.shadowMapEnabled = true;
 
-        this.quadNumbers = 20;
+        document.body.appendChild(this.renderer.domElement);
+    },
+
+    initMeshes: function() {
         this.planeGeometry = new THREE.PlaneGeometry(500, 500, this.quadNumbers, this.quadNumbers);
-        this.planeMaterial = new THREE.MeshBasicMaterial({color: 0x0000CC, wireframe: true});
+        // this.planeMaterial = new THREE.MeshLambertMaterial({color: 0x0000CC});
+        // this.planeMaterial = new THREE.MeshLambertMaterial({color: 0x111111});
+        this.planeMaterial = new THREE.MeshPhongMaterial({ambient: 0xCCCCCC, color: 0xCCCCCC, specular: 0xBBBBCC, shininess: 50, shading: THREE.SmoothShading});
+
         this.planeMesh = new THREE.Mesh(this.planeGeometry, this.planeMaterial);
-        this.planeMesh.rotation.set(Constants.planeAngleX, Constants.planeAngleY, Constants.planeAngleZ);
+        // this.lineMesh = new THREE.Mesh(this.planeGeometry, new THREE.MeshBasicMaterial({color: 0x00CCCC, wireframe: true}));
+        // this.lineMesh.position.z = -1;
+
+        this.planeMesh.castShadow = true;
+        this.planeMesh.receiveShadow = false;
 
         this.scene.add(this.planeMesh);
+        // this.scene.add(this.lineMesh);
 
         var vertices = this.planeGeometry.vertices;
 
         var middleVerticeIndex = this.quadNumbers * this.quadNumbers / 2;
 
         for(var i = 0; i < this.quadNumbers; i++) {
+            this.createMountain(this.quadNumbers * i + MathHelper.randInt(-10, 10), vertices, 30);
             if(i % 2) {
-                this.createMountain(this.quadNumbers + this.quadNumbers / 2 * i, this.planeGeometry.vertices, 120);
-                this.createMountain(this.quadNumbers * this.quadNumbers - this.quadNumbers / 2 * i, this.planeGeometry.vertices, 120);
+                this.createMountain(this.quadNumbers / 2 * i, vertices, 120);
+                this.createMountain(this.quadNumbers * this.quadNumbers - this.quadNumbers / 2 * i, vertices, 120);
             }
         }
-        this.createMountain(middleVerticeIndex, this.planeGeometry.vertices, 10);
+    },
 
-        this.controls = new THREE.TrackballControls(this.camera);
+    initLights: function() {
+        this.spotLight = new THREE.SpotLight(0xDCAF00);
+        this.spotLight.castShadow = true;
+        this.scene.add(this.spotLight);
+
+        this.ambientLight = new THREE.AmbientLight(0x221100);
+        this.ambientLight.z = 150;
+        this.scene.add(this.ambientLight);
     },
 
     createMountain: function(startVertice, vertices, maxHeight) {
@@ -65,6 +95,12 @@ Playground.prototype = {
         }
     },
 
+    customRender: function() {
+        var time = Date.now() * 0.0005;
+
+        this.spotLight.position.set(Constants.spotLightX, Constants.spotLightY, Constants.spotLightZ);
+    },
+
     render: function()
     {
         if(this.isDebug)
@@ -73,6 +109,9 @@ Playground.prototype = {
         }
 
         this.controls.update();
+
+        this.customRender();
+
         this.renderer.render(this.scene, this.camera);
 
         requestAnimationFrame(this.render.bind(this));
@@ -89,6 +128,11 @@ Playground.prototype = {
 
     initGui: function() {
         this.gui = new dat.GUI();
+        /*var spotLight = this.gui.addFolder('spotLight');
+        spotLight.open();
+        spotLight.add(Constants, 'spotLightX').min(-200).max(200);
+        spotLight.add(Constants, 'spotLightY').min(-200).max(200);
+        spotLight.add(Constants, 'spotLightZ').min(-200).max(200);*/
         /*var planeAngle = this.gui.addFolder('planeAngle');
         planeAngle.open();
         planeAngle.add(Constants, 'planeAngleX').min(-Math.PI).max(Math.PI).step(0.01);
